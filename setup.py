d0bea5e787cf5496b21978fd20c0cbb971875091
Drissa Samake <drissa.samake@gmail.com>
2024-08-29 18:07:59 +0200

    First commit to install module

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import setuptools
+
+setuptools.setup(
+    name='wqp',
+    version='1.0.0',
+    author='drissa.samake@gmail.com',
+    description='Wine quality predictor - a packaged machine learning algorithm to predict wine quality',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "scikit-learn==0.22.1",
+        "pandas==1.0.1"
+    ]
+)
